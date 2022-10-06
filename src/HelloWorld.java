103aa158b620342f1db365920ef5301bbdbc2d5d
OleksandrT <timoshenko1985@ukr.net>
2022-10-06 21:42:10 +0300

    add not used method

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,4 +2,8 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Git from IDE");
     }
+
+    public void printNamre() {
+        System.out.println("Ivan");
+    }
 }
