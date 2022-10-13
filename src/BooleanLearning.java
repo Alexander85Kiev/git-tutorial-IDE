499ffcb06bfcec858fbed760df4f7ecbe31c8c22
OleksandrT <timoshenko1985@ukr.net>
2022-10-13 21:08:18 +0300

    New Class String methods

--- src/BooleanLearning.java
+++ src/BooleanLearning.java
@@ -1,2 +1,59 @@
 public class BooleanLearning {
+
+
+
+
+        public static boolean weatherIsFine(){
+            return false;
+        }
+
+        public static boolean hasUmbrella(){
+            return false;
+        }
+
+    public static void main(String[] args) {
+        System.out.println(weatherIsFine() || hasUmbrella());
+    }
+
+    // || OR - логічне АБО
+    // Ми пыдемо гуляти якщо:
+    // а - буде гарна погода
+    // АБО
+    // б - в нас е парасолька
+
+    // a = true,b = false      |    true
+    // a = true, b = true      |    true
+    // a = false, b = true     |    true
+    // a = false, b = false    |    false
+
+    // true - коли хоч один із операндів повертає true
+
+     //&& - Логічне  I  I
+    //  a - Ми отримаємо сертифікат GOIt якщо будемо ходити на всі лекції
+    // I
+    //  b -   виконаємо всі дзю
+
+    // a = true,b = false      |    false
+    // a = true, b = true      |    true
+    // a = false, b = true     |    false
+    // a = false, b = false    |    false
+
+    // false - коли хоч один із операндів повертає false
+
+
+    // ^ XOR - виключаєче АБО
+
+    // a = true,b = false      |   true
+    // a = true, b = true      |   false
+    // a = false, b = true     |   true
+    // a = false, b = false    |   false
+
+    // ! - заперечення
+
+    // !true => false
+    // !false => true
+
+
+
+
 }
--- src/StringLearning.java
+++ src/StringLearning.java
@@ -1,2 +1,26 @@
 public class StringLearning {
+
+
+    public static void main(String[] args) {
+        System.out.println("AAA".equals("AAAAA"));
+        String input = "AaAAaAAA";
+        System.out.println(input.length());
+        System.out.println(input.toLowerCase());
+        System.out.println("Hello " + "how are you");
+        System.out.println(String.join(" ", "aaa","cccc","bbbbbbb"));
+        System.out.println("aaaaa".concat("BBBBB"));
+        System.out.println("Java".contains("a"));
+        System.out.println("I learn Java".charAt(5));
+        System.out.println("I learn Java".startsWith("I "));
+        System.out.println("I learn Java".endsWith("va"));
+
+
+        System.out.println("I am C++ dev".replace("C++","Java"));
+        System.out.println("   Java  !  ".trim());
+        System.out.println("I am C++ dev".substring(5));
+        System.out.println("I am C++ dev".substring(5,9));
+
+    }
+
+
 }
