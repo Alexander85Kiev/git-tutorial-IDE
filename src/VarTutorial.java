6ce8090f2ac501eb6342df80e1ce0d046eae5ce6
OleksandrT <timoshenko1985@ukr.net>
2022-10-13 22:07:55 +0300

    some updates

--- src/VarTutorial.java
+++ src/VarTutorial.java
@@ -1,2 +1,20 @@
 public class VarTutorial {
+    public static void main(String[] args) {
+        var a = 3;
+        var b = 12341L;
+        var c =  1.5;
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("I'm in Cycle");
+        }
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+
+
+    }
+
+
+
 }
